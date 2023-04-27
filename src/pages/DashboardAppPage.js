05eeeed76c6c44e8f5c78cf852702ea2db001717
Lurian Ortiz <lurian502@gmail.com>
2023-04-27 18:18:50 -0300

    Primeiro commit

---
+++ src/pages/DashboardAppPage.js
@@ -0,0 +1,11 @@
+// import { useTheme } from '@mui/material/styles';
+
+export default function DashboardAppPage() {
+  // const theme = useTheme();
+
+  return (
+    <>
+      <h1>Dash</h1>
+    </>
+  );
+}
